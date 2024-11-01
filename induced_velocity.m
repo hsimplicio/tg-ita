2607d48929275e1be6b1a1e9aa33060b8f37cbd9
Henrique Simplicio <henrique.simplicio@delfos.energy>
2024-11-02 00:30:57 +0100

    fix induced velocity at hover

--- induced_velocity.m
+++ induced_velocity.m
@@ -12,12 +12,19 @@ function vi = induced_velocity(vh, V, alpha)
         alpha double
     end
 
-    vi_func = @(w,vh,V,alpha) w - vh .^ 2 ./ (sqrt((V .* cos(alpha)) .^ 2) + (V .* sin(alpha) + w) .^ 2);
-
-    options = optimset('TolX', 1e-6);
+    % For hover (V=0), vi should equal vh
+    hover_condition = (V < 1e-6);
 
     vi = zeros(1,length(vh));
-    for i = 1:length(vh)
+    vi(hover_condition) = vh(hover_condition);
+
+    vi_func = @(w,vh,V,alpha) w - vh .^ 2 ./ (sqrt((V .* cos(alpha)) .^ 2) + (V .* sin(alpha) + w) .^ 2);
+
+    % For non-hover cases, use fzero
+    non_hover = ~hover_condition;
+    options = optimset('TolX', 1e-6);
+
+    for i = find(non_hover)
         vi(i) = fzero(@(w) vi_func(w,vh(i),V(i),alpha(i)), [-50,50], options);
     end
 end

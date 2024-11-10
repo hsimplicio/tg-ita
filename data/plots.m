ce72c4498d273fa0fb5c97e43c8d66001dbd6ed2
Henrique Simplicio <henrique.simplicio@delfos.energy>
2024-11-10 11:45:04 +0100

    add plots

--- data/plots.m
+++ data/plots.m
@@ -1,16 +1,23 @@
-t = readmatrix('psopt/tt0.dat');
-x = readmatrix('psopt/xt0.dat');
-u = readmatrix('psopt/ut0.dat');
-XY = readmatrix('psopt/XY.dat');
+addpath('..');
 
-figure;
-plot(t, x(1,:));
+% Plot PSOPT solution
+t = readmatrix('../data/psopt/tt0.dat');
+x = readmatrix('../data/psopt/xt0.dat');
+u = readmatrix('../data/psopt/ut0.dat');
+XY = readmatrix('../data/psopt/XY.dat');
 
-solution = load('solution-base-t45.mat');
 z = struct();
 z.time = t;
 z.state = x;
 z.control = u;
 
-addpath('../evtol');
+plotEvtolResults('PSOPT', z);
+
+% Plot MATLAB solution
+solution = load('solution-base-t45.mat');
+z = struct();
+z.time = linspace(0, 45, size(solution.z, 2));
+z.state = solution.z(1:5,:);
+z.control = solution.z(6:end,:);
+
 plotEvtolResults('Base', z);
